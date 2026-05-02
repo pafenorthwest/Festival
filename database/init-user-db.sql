67b9717e606f3d0da0de748129af1bb61dbafc50
Eric Passmore <eric.passmore@gmail.com>
2026-05-02 14:04:32 -0700

    add schema name to default search path for psql

--- database/init-user-db.sql
+++ database/init-user-db.sql
@@ -2,5 +2,4 @@ CREATE ROLE festivaladmin LOGIN PASSWORD '<strong_password>'
 CREATE DATABASE festival_db;
 GRANT CONNECT ON DATABASE festival_db TO festivaladmin;
 GRANT CREATE ON DATABASE festival_db TO festivaladmin;
-
-
+ALTER DATABASE festival_db SET search_path TO <schema_env>, public;
